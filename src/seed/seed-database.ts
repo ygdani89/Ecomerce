305f97123ab8b7e116dae288527ea10c22b1b0fe
ygdani89 <ygdani89@outlook.com>
2025-12-22 08:14:26 -0500

    data base 1.4
    problem fixed

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -1,16 +1,40 @@
 import { create } from 'zustand';
-import initialData  from './seed'
+import initialData from './seed'
 import prisma from '../lib/prisma';
+import { log } from 'console';
 
 
 
 async function main() {
 
   // 1. Borrar registros previos
-    await prisma.productImage.deleteMany();
-  
-  
-  console.log( 'Seed ejecutado correctamente' );
+  await Promise.all([
+
+    prisma.productImage.deleteMany(),
+    prisma.category.deleteMany(),
+    prisma.product.deleteMany()
+  ]);
+
+  // await prisma.category.create({
+  //   data: {
+  //     name: 'Shirts'
+  //   }
+  // })
+
+  const { categories, products } = initialData
+
+  const categoriesDAta = categories.map(name => ({ name }));
+
+
+  await prisma.category.createMany({
+
+    data: categoriesDAta
+  }
+  )
+
+
+
+  console.log('Seed ejecutado correctamente');
 }
 
 
@@ -21,10 +45,10 @@ async function main() {
 
 
 
-( () => {
+(() => {
 
-  if ( process.env.NODE_ENV === 'production' ) return;
+  if (process.env.NODE_ENV === 'production') return;
 
 
   main();
-} )();
+})();
